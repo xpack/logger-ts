578f5491a86175fde0d7787159855d5b1d081849
Liviu Ionescu <ilg@livius.net>
2023-03-01 22:48:09 +0200

    add src/index.mjs

---
+++ src/index.mjs
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the xPack project (http://xpack.github.io).
+ * Copyright (c) 2017 Liviu Ionescu. All rights reserved.
+ *
+ * Licensed under the terms of the MIT License.
+ * See LICENSE in the project root for license information.
+ */
+
+/* eslint max-len: [ "error", 80, { "ignoreUrls": true } ] */
+
+// ----------------------------------------------------------------------------
+
+// Re-export the library definitions.
+export * from './dist/lib/logger.js'
+
+// ----------------------------------------------------------------------------
