00d6d31f87180eabd05dc3c79847ff7556b70069
Liviu Ionescu <ilg@livius.net>
2024-11-01 23:45:43 +0200

    website: remove docs/api content

--- website/docs/api/typedoc-sidebar.cjs
+++
@@ -1,4 +0,0 @@
-// @ts-check
-/** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
-const typedocSidebar = { items: [{"type":"category","label":"Classes","items":[{"type":"doc","id":"api/classes/Logger","label":"Logger"}]},{"type":"category","label":"Type Aliases","items":[{"type":"doc","id":"api/type-aliases/LogLevel","label":"LogLevel"}]}]};
-module.exports = typedocSidebar.items;
