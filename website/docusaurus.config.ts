d3c5535a941ffcef91420a3f9f0f3dd2fd6ab9d0
Liviu Ionescu <ilg@livius.net>
2024-12-24 17:02:09 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -247,6 +247,9 @@ const config: Config = {
     }
   ],
 
+  // No longer needed.
+  // themes: [ '@docusaurus/theme-search-algolia' ],
+
   themeConfig: {
     // The project's social card, og:image, twitter:image, 1200x630
     image: 'img/sunrise-og-image.jpg',
@@ -275,7 +278,7 @@ const config: Config = {
         },
         {
           type: 'dropdown',
-          label: 'Documentation',
+          label: 'Getting Started',
           to: 'docs/getting-started',
           position: 'left',
           items: [
@@ -398,7 +401,7 @@ const config: Config = {
             },
             {
               label: 'Discord',
-              href: 'https://discord.gg/kbzWaJerFG',
+              href: 'https://discord.com/invite/kbzWaJerFG',
             },
             {
               label: 'X/Twitter',
@@ -433,6 +436,25 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+    },
+    algolia: {
+      // https://docsearch.algolia.com/docs/docsearch-v3/
+      appId: "KIDD7R4CL1",
+      apiKey: "ca2ffc431941284609f2d50202fc5506",
+      indexName: "xpackio",
+
+      // It ensures that search results are relevant to the current
+      // language and version. Enabled by default.
+      contextualSearch: false, // true,
+      // Optional: Replace parts of the item URLs from Algolia.
+      // Useful when using the same search index for multiple deployments
+      // using a different baseUrl. You can use regexp or string in the
+      // `from` param. For example: localhost:3000 vs myCompany.com/docs
+      // replaceSearchResultPathname: undefined,
+      // Optional: Algolia search parameters
+      searchParameters: {},
+      // Optional: path for search page that enabled by default (`false` to disable it)
+      searchPagePath: false, // 'search',
     },
   } satisfies Preset.ThemeConfig,
 
