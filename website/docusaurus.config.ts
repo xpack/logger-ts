ec588f929aa5a5e52e5672db68482cfcf8a8ee85
Liviu Ionescu <ilg@livius.net>
2024-12-26 13:19:11 +0200

    website: updates

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -348,19 +348,29 @@ const config: Config = {
           'aria-label': 'GitHub repository',
         },
         {
-          label: `v${customFields.releaseVersion}`,
+          type: 'dropdown',
+          href: 'https://github.com/xpack/logger-ts/',
+          position: 'right',
+          label: 'GitHub',
+          items: [
+            {
+              label: `logger-ts`,
+              href: `https://github.com/xpack/logger-ts/`,
+            },
+            {
+              label: 'xpack',
+              href: 'https://github.com/xpack/',
+            },
+            {
+              label: 'xpack-dev-tools',
+              href: 'https://github.com/xpack-dev-tools/',
+            },
+          ]
+        },
+        {
+          label: `${customFields.releaseVersion}`,
           position: 'right',
           href: `https://www.npmjs.com/package/@xpack/logger/v/${customFields.releaseVersion}`,
-        },
-        {
-          href: 'https://github.com/xpack/',
-          label: 'xpack',
-          position: 'right',
-        },
-        {
-          href: 'https://github.com/xpack-dev-tools/',
-          label: 'xpack-dev-tools',
-          position: 'right',
         },
       ],
     },
