957a9d17249dc911adf7a0b716813b7e5accfbb6
Liviu Ionescu <ilg@livius.net>
2025-01-21 15:19:30 +0200

    website: re-generate commons

--- website/src/pages/index.tsx
+++ website/src/pages/index.tsx
@@ -36,7 +36,7 @@ function HomepageHeader() {
         <p className="hero__subtitle">{siteConfig.tagline}</p>
 
         <div className={styles.installWithCopy}>
-          <InstallWithCopy>npm install @xpack/logger@{customField('releaseVersion')}</InstallWithCopy>
+          <InstallWithCopy>npm install @xpack/logger@6.0.0</InstallWithCopy>
         </div>
 
       </div>
